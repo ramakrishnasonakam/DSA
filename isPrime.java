bf7e70f292516d8b1a80394bb8d965f7e8f9459e
Ramakrishna Sonakam <37579191+ramakrishnasonakam@users.noreply.github.com>
2021-05-23 22:04:09 +0530

    Update isPrime.java

--- isPrime.java
+++ isPrime.java
@@ -3,7 +3,7 @@ class Main {
      static boolean isPrime(int a) {
         if (a<=1) {
             return false;
-        } // end of if n is less than 4; 3...
+        } // end of if
         for (int i=2; i<a; i++) {
             if (a % i == 0) {
                 return false;
