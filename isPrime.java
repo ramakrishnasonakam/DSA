f2ec9d9bb24c53ac93f82b68d1057d48e7c60b69
Ramakrishna Sonakam <37579191+ramakrishnasonakam@users.noreply.github.com>
2021-05-23 21:35:16 +0530

    Add files via upload

---
+++ isPrime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Main {
+     static boolean isPrime(int a) {
+        if (a<=1) {
+            return false;
+        } // end of if n is less than 4; 3...
+        for (int i=2; i<a; i++) {
+            if (a % i == 0) {
+                return false;
+            }// end of if block
+        }// end of for block
+        return true;
+    
+    }//end of isPrime
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        System.out.println("Enter the integer: ");
+        a = sc.nextInt();
+        if (isPrime(a)) {
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+
+
+    }// end of void main
+}// end of class Main
