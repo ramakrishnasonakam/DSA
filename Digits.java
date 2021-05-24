6eb4d09f420526861552514da1d42874d134b625
Ramakrishna Sonakam <37579191+ramakrishnasonakam@users.noreply.github.com>
2021-05-24 18:31:09 +0530

    Add files via upload

---
+++ Digits.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Digits {
+    public static void countDigits(int x) {
+        if (x == 0) {
+            return;
+        }// end of base case    
+        int count = 0;
+        while (x > 0){
+            x = x / 10;
+            count++;
+        }
+        System.out.println(count);
+        // end of while
+    }// end of countDigits
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int x = sc.nextInt();
+        countDigits(x);
+    }//end of void main
+}// end of Digits
