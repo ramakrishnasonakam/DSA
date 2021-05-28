0dbe812e6fc00ae905ef32e7bff91f8a94cf4b94
Ramakrishna Sonakam <37579191+ramakrishnasonakam@users.noreply.github.com>
2021-05-28 22:47:59 +0530

    Update Digit.java

--- Digit.java
+++ Digit.java
@@ -1,16 +1,21 @@
+/*
+Printing the digits in the given integer from
+left to right order.
+*/
+
 import java.util.Scanner;
 class Digit {
     public static void printDigit(int x){
         int temp = x;
         int div = 1;
         if (x<10){
-            // Sysste.;
+           return x;
         }// end of base case
         while(temp > 0) {
             temp = temp/10;
             div = div * 10;
         }// end of while block
-        System.out.println(div);
+//         System.out.println(div);
         while(x > 0){
             div = div / 10;
             System.out.println(x/div); 
